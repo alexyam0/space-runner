19da51cd18cc6378f178f44911d71c400eb3f438
alexyam0 <alexyam001@gmail.com>
2023-04-17 21:44:52 -0700

    more setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { useState, Suspense } from 'react'
+import { useRef, Suspense } from 'react'
 import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 
 function App() {
-  
+  const boxRef = useRef();
+
+  // useFrame((state, delta) => {
+  //   boxRef.current.rotation.y += delta
+  // })
 
   return (
-    <Suspense fallback={null}>
-      <Canvas>
-        <mesh position={[0, 0, 0]} scale={1.5} >
+    // <Suspense fallback={null}>
+    //   <Canvas>
+    <>
+        <PerspectiveCamera position={[0, 0, 0]} fov={10} />
+        <OrbitControls target={[0, 0, 0]} />
+
+        <mesh ref={ boxRef } position={[0, 0, 0]} scale={1.5} >
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color='mediumpurple' />
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
+          <ambientLight intensity={ 0.75 } />
+          <pointLight position={[1, 2, 3]} />
         </mesh>
-        <mesh position-y= { -1} rotation-x={ -Math.PI * 0.5 } scale={ 10 } >
+
+        <mesh position-y= { -1 } rotation-x={ -Math.PI * 0.5 } scale={ 10 } >
           <planeGeometry />
-          <meshBasicMaterial color='greenyellow' />
+          <meshStandardMaterial color='greenyellow' />
         </mesh>
-        <PerspectiveCamera position={[0, 0, 0]} fov={50} />
-        <OrbitControls target={[0, 0, 0]} />
+    </>
         
-        
-      </Canvas>
-    </Suspense>
+    //   </Canvas>
+    // </Suspense>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx';
 import './index.css';
+import { Canvas } from '@react-three/fiber';
+
+const cameraSettings = {
+    fov: 75,
+    near: 0.1,
+    far: 200,
+    position: [ 3, 1, 4]
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Canvas
+        camera={ cameraSettings }
+    >
+        <App />
+    </Canvas>
+   
 );
